4dc9b03ae5975bb7e457f36608046a83f305caad
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-05-09 10:21:40 +0300

    added volume control

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useRef, useCallback } from 'react';
-const Key = ({ keyItem, bank, power }) => {
+const Key = ({ keyItem, bank, power, volume }) => {
   const audioRef = useRef(null);
   const playAudio = useCallback(() => {
     if (power) {
       if (audioRef.current) {
+        console.log(audioRef.current.volume);
         audioRef.current.play();
       }
     }
   });
   useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume / 10;
+    }
     window.addEventListener('keydown', e => {
       if (keyItem.key === e.key) {
         playAudio();
       }
     });
-  }, [bank, keyItem, keyItem.key, playAudio]);
+  }, [bank, keyItem, keyItem.key, playAudio, volume]);
   return (
     <>
       <audio src={keyItem[bank]} ref={audioRef} />
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import Key from './Key';
 import keyList from '../keyList';
-const Keypad = ({ bank,power }) => {
+const Keypad = ({ bank, power }) => {
   const [keys, setKeys] = useState(keyList);
+  const [volume, setVolume] = useState(1);
   return (
     <>
+      <input
+        onChange={e => setVolume(e.target.value)}
+        type="range"
+        min="1"
+        max="10"
+        value={volume}
+      />
       {keys.length > 0
         ? keyList.map(keyItem => (
-            <Key key={keyItem.key} keyItem={keyItem} bank={bank} power={power} />
+            <Key
+              volume={volume}
+              key={keyItem.key}
+              keyItem={keyItem}
+              bank={bank}
+              power={power}
+            />
           ))
         : ''}
     </>
