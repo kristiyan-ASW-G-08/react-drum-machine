f095a6fe7d2539e159a8a4d9c507aa1f48f75914
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-07-15 19:27:23 +0300

    display the current bank

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import Keypad from './components/Keypad';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState } from "react";
+import Keypad from "./components/Keypad";
+import logo from "./logo.svg";
+import "./App.scss";
 
 const App = () => {
   const [power, setPower] = useState(true);
-  const [bank, setBank] = useState('firstBank');
+  const [bank, setBank] = useState("firstBank");
 
   const togglePower = () => {
-    console.log('power', power);
+    console.log("power", power);
     setPower(power => !power);
   };
   const toggleBank = () => {
-    const nextBank = bank === 'firstBank' ? 'secondBank' : 'firstBank';
+    const nextBank = bank === "firstBank" ? "secondBank" : "firstBank";
     setBank(nextBank);
   };
   return (
     <div className="App">
       <div id="drum-machine">
         <button onClick={togglePower}>Power</button>
-        <button onClick={toggleBank}>Bank</button>
+        <button onClick={toggleBank}>
+          Bank {bank === "firstBank" ? "1" : "2"}
+        </button>
         <Keypad bank={bank} power={power} />
       </div>
     </div>
